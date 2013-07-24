634fbb23b4d35012bd1a7e2f64904f611849bd31
lmeyerov <lmeyerov@gmail.com>
2013-07-24 00:55:36 -0700

    Get sentence hints and highlight bad ratings on page

--- copyedit/server/server.js
+++ copyedit/server/server.js
@@ -44,11 +44,14 @@ function fetchGet (url, sent, sentenceI, jobID) {
 		} else if (response.statusCode == 404) {
 			console.log('404, try again in ' + (delay / 1000) + ' seconds...');
 			setTimeout(function () { fetchGet(url, sent, sentenceI, jobID); }, delay);
+		} else if (response.statusCode == 502) {
+			console.log('502 (down), try again in ' + (delay / 1000) + ' seconds...'); 
+			setTimeout(function () { fetchGet(url, sent, sentenceI, jobID); }, delay);
 		} else {
 			try {
-				var out = JSON.parse(body)[0];				
+				var out = JSON.parse(body);
 				console.log('Result');
-				console.log(out, sent);
+				console.log(body, sent);
 				updateBatch(jobID, sentenceI, out);
 			} catch (exn) {
 				console.log('error', err);
@@ -76,6 +79,54 @@ app.get('/api/improvements', function (req, res) {
 	res.send(JSON.stringify(out));	
 }); 
 
+
+function askAsRadio (sent) {
+	return qs.stringify({
+				distinctUsers: 2,
+				instructions: //task description
+					'Proofread: at first glance, which sentences have spelling or grammar mistakes?\nAnswer for each sentence and ignore formatting hints such as "\\section{Chapter name}" and "\\ref{??}"', 
+				question: 
+					JSON.stringify({Radio: {
+						questionText: '"' + sent + '"',
+						chooseOne: ['mistake','ok']
+					}}),
+				knownAnswerQuestionsIgnore: JSON.stringify({
+					answeredQuestions: [
+						{question: 
+							{Radio: {questionText: "The schedule is combined with the attribute grammar to form an intermediate representation, and different code generators target different backends such as JavaScript, OpenCL, and C++.",
+									 chooseOne: ['mistake','ok']}},
+						answer: 'ok'}, 
+						{question: 
+							{Radio: {questionText: "Many of the difficulty in computer science stem from handling loops.",
+									 chooseOne: ['mistake','ok']}},
+						 answer: 'mistake'}], //difficulties => difficulty
+					percentCorrect: 100
+				})
+			});
+}
+
+function askAsText (sent) {
+	return qs.stringify({
+				distinctUsers: 3,
+				instructions: //task description
+					'Proofread: at first glance, which sentences have spelling or grammar mistakes?\nIf there is a mistake, briefly describe it. Otherwise, leave it described as "ok". You can ignore formatting hints such as "\\section{Chapter name}" and "\\ref{??}".', 
+				question: 
+					JSON.stringify({Text: {
+						questionText: '"' + sent + '"',
+						defaultText: 'ok'
+					}}),
+				knownAnswerQuestionsIgnore: JSON.stringify({
+					answeredQuestions: [
+						{question: 
+							{Radio: {questionText: "The schedule is combined with the attribute grammar to form an intermediate representation, and different code generators target different backends such as JavaScript, OpenCL, and C++.",
+									 defaultText: 'ok'}},
+						 answer: 'ok'}],
+					percentCorrect: 100
+				})
+			});
+}
+
+
 app.post('/api/turkit', function (req, res) {
 	var sents = req.body.sentences ? req.body.sentences : [];
 	var batchID = req.body.batchID;
@@ -84,15 +135,7 @@ app.post('/api/turkit', function (req, res) {
 	console.log('got sents', sents);
 	
 	function buildUrl (sent) {
-		return 'http://vivam.us/human/ask?' + qs.stringify({
-			instructions: //task description
-				'Copy editing: are there spelling or grammar mistakes in the following sentences? Ignore style formatting hints such as "\section{Chapter name.}" and "\ref{??}"', 
-			question: 
-				JSON.stringify({Radio: {
-					questionText: sent,
-					chooseOne: ['yes','no']
-				}})
-			});
+		return 'http://vivam.us/human/ask?' + askAsText(sent);
 	}	
 	
 	sents.map(function (sent, idx) {
